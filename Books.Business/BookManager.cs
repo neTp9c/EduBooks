5a3225d94b95ef32bac912298d506f608c97aa13
Alex Petryakov <alexpetryakov@gmail.com>
2016-08-14 15:44:05 +0300

    Add simple Book Manager class

---
+++ Books.Business/BookManager.cs
@@ -0,0 +1,45 @@
+﻿using Books.Data;
+using Books.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Business
+{
+    public class BookManager
+    {
+        private readonly BookContext _booksContext;
+
+        public BookManager()
+        {
+            _booksContext = new BookContext();
+        }
+
+        public void Create(Book book)
+        {
+            _booksContext.Books.Add(book);
+        }
+
+        public void Update(Book book)
+        {
+            _booksContext.Books.Update(book);
+        }
+
+        public void Delete(Book book)
+        {
+            _booksContext.Books.Remove(book);
+        }
+
+        public Book GetBook(int id)
+        {
+            return _booksContext.Books.SingleOrDefault(b => b.Id == id);
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return _booksContext.Books.ToList();
+        }
+    }
+}
