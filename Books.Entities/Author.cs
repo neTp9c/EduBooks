768a3183ab0caa04bb4d1fe3479ce8ad9ca8db75
Alex Petryakov <alexpetryakov@gmail.com>
2016-08-14 14:14:44 +0300

    Add base entities

---
+++ Books.Entities/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
---
+++ Books.Entities/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public int Title { get; set; }
+        public int PageCount { get; set; }
+        public int PublicationYear { get; set; }
+        public string ISBN { get; set; }
+        public byte[] Image { get; set; }
+
+
+        public virtual ICollection<Publisher> Publisher { get; set; }
+        public virtual ICollection<Author> Authors { get; set; }
+    }
+}
