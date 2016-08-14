156897cdf45fe0339c585c2b5ee1f73681bd23ab
Alex Petryakov <alexpetryakov@gmail.com>
2016-08-14 15:46:51 +0300

    Add owin, webapi, books controller
    Add simple ef call (through books manager for testing ef)

---
+++ Books.Api/Controllers/BooksController.cs
@@ -0,0 +1,46 @@
+﻿using Books.Business;
+using Books.Entities;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Books.Api.Controllers
+{
+    
+    public class BooksController : ApiController
+    {
+        private readonly BookManager _bookManager;
+
+        public BooksController()
+        {
+            _bookManager = new BookManager();
+        }
+
+        // GET api/<controller>
+        public IEnumerable<Book> Get()
+        {
+            var books = _bookManager.GetBooks();
+            return books;
+        }
+
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Books.Api/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Books.Api.Startup))]
+
+namespace Books.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config);
+        }
+    }
+}
