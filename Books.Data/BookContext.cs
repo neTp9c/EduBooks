27936ca71aa0df16f2a4b6319be7527eead5d411
Alex Petryakov <alexpetryakov@gmail.com>
2016-08-14 15:43:13 +0300

    Add DbContext class (ef core for in memory usage)

---
+++ Books.Data/BookContext.cs
@@ -0,0 +1,29 @@
+﻿using Books.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Books.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseInMemoryDatabase();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+    }
+}
